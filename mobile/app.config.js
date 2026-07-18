d885180278822fcbb384d1bd44ce254125ff7344
ismlahmt <ismailahmetsimsek@gmail.com>
2026-07-18 22:46:46 +0300

    güncelleme 3

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -14,7 +14,7 @@ module.exports = {
     android: {
       package: IS_PREVIEW ? "com.jesuisapres.teleremo.test" : "com.jesuisapres.teleremo",
       adaptiveIcon: {
-        backgroundColor: IS_PREVIEW ? "#4B5563" : "#0F172A", // Test uygulamasının arka planı gri olsun karışmasın
+        backgroundColor: IS_PREVIEW ? "#4B5563" : "#000000", // Gerçek APK'da tam siyah
         foregroundImage: "./assets/adaptive-icon.png"
       },
       predictiveBackGestureEnabled: false
@@ -22,6 +22,30 @@ module.exports = {
     web: {
       favicon: "./assets/icon.png"
     },
+    splash: {
+      image: "./assets/icon.png",
+      resizeMode: "contain",
+      backgroundColor: IS_PREVIEW ? "#4B5563" : "#000000"
+    },
+    updates: {
+      url: "https://u.expo.dev/ac7874c0-8b09-4fbf-a8bc-9b92e2783972"
+    },
+    extra: {
+      eas: {
+        projectId: "ac7874c0-8b09-4fbf-a8bc-9b92e2783972"
+      }
+    },
+    plugins: [
+      "expo-font",
+      [
+        "expo-build-properties",
+        {
+          "android": {
+            "usesCleartextTraffic": true
+          }
+        }
+      ]
+    ],
     runtimeVersion: "1.0.0"
   }
 };
